285079776dced440048f22c3876f747e70c051ff
bafanzul <bayu_23520004@stimata.ac.id>
2024-03-03 12:07:36 +0700

    update css

--- Latihan1.php
+++ Latihan1.php
@@ -1,13 +1,47 @@
 <?php
-// Lingkup variabel
 
-$angka = 10;
-function Angka(){
-    $angka = 20;
-    echo $angka;
-}
+$mahasiswa = [
+    [
+        "nim" => "2352004",
+        "nama" => "Fangki Zulkarnain",
+        "jurusan" => "Teknologi Informasi",
+        "email" => "bayu_23520004@stimata.ac.id",
+        "img" =>  "img1.png"
+    ],
+    [
+        "nim" => "23520001",
+        "nama" => "Suci Wulandari",
+        "jurusan" => "Teknologi Informasi",
+        "email" => "Suciwulan@stimata.ac.id",
+        "img" =>  "img2.jpg"
+    ],
+    [
+        "nim" => "23520005",
+        "nama" => "dwi",
+        "jurusan" => "Teknologi Informasi",
+        "email" => "dwi@stimata.ac.id",
+        "img" =>  "img3.jpg"
+    ],
 
-echo $angka;
-echo "<br>";
-Angka();
+];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Latihan GET</title>
+    </head>
+    <body>
+        <h1>Daftar Mahasiswa</h1>
+        <?php foreach ($mahasiswa as $mhs) : ?>
+            <ul>
+                <li>
+                    <a href="latihan2.php?nama=<?php echo $mhs['nama']; ?>&nim=<?php echo $mhs['nim']; ?>&jurusan=<?php echo $mhs['jurusan']; ?>&email=<?php echo $mhs['email']; ?>&img=<?php echo $mhs['img']; ?>" > <?php echo $mhs['nama']; ?></a>
+                </li>
+            </ul>
+        <?php endforeach; ?>
+    </body>
+</html>
