4c77582503be36d2686fe102b0490d083441c3b8
bafanzul <bayu_23520004@stimata.ac.id>
2024-03-07 19:53:36 +0700

    php

---
+++ Latihan4.php
@@ -0,0 +1,19 @@
+<?php
+if (!isset ($_POST["submit"])){
+    //redirect
+    header("Location: Latihan3.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil Metode POST</title>
+</head>
+<body>
+    <h1>Selamat Datang, <?= $_POST["nama"];  ?> </h1>
+</body>
+</html>
